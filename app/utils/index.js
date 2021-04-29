76a9bf3ace5cc49683ada4f05949fc86a15e7545
rcortez <renecortez@banpro.com.ni>
2021-04-29 10:18:00 -0600

    Create util to remove extension fro files

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,7 +1,9 @@
+const { removeExtensionFromFile } = require('./removeExtensionFromFile')
 const { buildErrObject } = require('./buildErrObject')
 const { handleError } = require('./handleError')
 
 module.exports = {
+  removeExtensionFromFile,
   buildErrObject,
   handleError
 }
