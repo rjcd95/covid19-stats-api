f8286b445ec2c303d1e867e31235d8d7ad4d33da
rcortez <renecortez@banpro.com.ni>
2021-04-29 17:55:44 -0600

    Create utils validations

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,9 +1,13 @@
 const { removeExtensionFromFile } = require('./removeExtensionFromFile')
 const { buildErrObject } = require('./buildErrObject')
 const { handleError } = require('./handleError')
+const { itemNotFound } = require('./itemNotFound')
+const { validateResult } = require('./validateResult')
 
 module.exports = {
   removeExtensionFromFile,
   buildErrObject,
-  handleError
+  handleError,
+  itemNotFound,
+  validateResult
 }
