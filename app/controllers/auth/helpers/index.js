285e406b889183318f23942979f159620be7a69c
rcortez <renecortez@banpro.com.ni>
2021-04-30 12:41:04 -0600

    Add helper to find user by email

--- app/controllers/auth/helpers/index.js
+++ app/controllers/auth/helpers/index.js
@@ -2,10 +2,12 @@ const { generateToken } = require('./generateToken')
 const { registerUser } = require('./registerUser')
 const { setUserInfo } = require('./setUserInfo')
 const { returnRegisterToken } = require('./returnRegisterToken')
+const { findUser } = require('./findUser')
 
 module.exports = {
   generateToken,
   registerUser,
   setUserInfo,
   returnRegisterToken,
+  findUser
 }
