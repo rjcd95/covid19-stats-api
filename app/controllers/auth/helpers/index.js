16c9a4366ae207e8fcdbd00f208c5fcb8dc80b99
rcortez <renecortez@banpro.com.ni>
2021-04-30 12:47:44 -0600

    Check if user is blocked or not

--- app/controllers/auth/helpers/index.js
+++ app/controllers/auth/helpers/index.js
@@ -3,11 +3,13 @@ const { registerUser } = require('./registerUser')
 const { setUserInfo } = require('./setUserInfo')
 const { returnRegisterToken } = require('./returnRegisterToken')
 const { findUser } = require('./findUser')
+const { userIsBlocked } = require('./userIsBlocked')
 
 module.exports = {
   generateToken,
   registerUser,
   setUserInfo,
   returnRegisterToken,
-  findUser
+  findUser,
+  userIsBlocked
 }
--- app/controllers/auth/login.js
+++ app/controllers/auth/login.js
@@ -1,4 +1,7 @@
-const { findUser } = require('./helpers')
+const { 
+  findUser,
+  userIsBlocked
+} = require('./helpers')
 const { handleError } = require('../../utils')
 
 /**
@@ -10,6 +13,8 @@ const login = async (req, res) => {
   try {
     const data = req.body;
     const user = await findUser(data.email)
+    //Validate if user is blocked or not
+    await userIsBlocked(user)
     console.log(user);
     res.status(200).json({msg: 'login'})
   } catch (error) {
